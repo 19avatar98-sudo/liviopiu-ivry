f14528c64ffaa7edd09ebea8cf3fe3b6a806da70
Livio Piu <19avatar98@gmail.com>
2026-01-24 13:04:19 +0100

    3 mudança

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,5 @@
+import RouletteClient from "./roleta/RouletteClient";
+
 export default function Page() {
-  return (
-    <div style={{ padding: 40 }}>
-      <h1>Roue des cadeaux</h1>
-      <p>Le site est bien lancé 🎉</p>
-    </div>
-  );
+  return <RouletteClient />;
 }
