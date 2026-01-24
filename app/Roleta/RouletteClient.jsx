e2a03210eb838585922efd24552fa902ea29f993
Livio Piu <19avatar98@gmail.com>
2026-01-24 13:02:27 +0100

    2 mudança

---
+++ app/Roleta/RouletteClient.jsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function RouletteClient() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h2>🎡 Roue des cadeaux</h2>
+      <p>La roleta arrive ici.</p>
+    </div>
+  );
+}
